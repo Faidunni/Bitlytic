9c1618f8a6bc764aa9c603ab49435d48fad4d687
Faidunni <hazeezfaizat@gmail.com>
2024-10-23 08:36:05 +0100

    removed unused icon

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -1,6 +1,5 @@
 import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import { IoToggle } from "react-icons/io5";
 import { Moon, SunMoon } from "lucide-react";
 
 import {
